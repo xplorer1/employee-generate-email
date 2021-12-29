1a54f9eee5f59b917c27320016b81878049acdcf
Chijioke Ugwuanyi <Chijioke.Ugwuanyi@Interswitchng.com>
2021-12-29 10:35:16 +0100

    Added getter and setter methods.

--- src/EmployeeActions.java
+++ src/EmployeeActions.java
@@ -1,34 +1,65 @@
+import java.util.Scanner;
 import java.util.UUID;
 
 public class EmployeeActions {
-    String firstName;
-    String lastName;
-    String department;
-    String password;
-    int mailBoxCapacity;
-    final String companyName = "Microsoft Inc.";
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String password;
+    private int mailBoxCapacity;
+    private final String companyName = "Microsoft Inc.";
 
-    public EmployeeActions(String firstName, String lastName, String department) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.department = department;
+    private String generateEmail() {
+        String [] compName = this.companyName.split(" ");
+        return this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@" + this.department.toLowerCase() + "." + compName[0].toLowerCase() + ".com";
     }
 
-    String generateEmail() {
-        return this.firstName + "." + this.lastName + "@" + this.department + "." + this.companyName + ".com";
-    }
-
-    String generatePassword() {
+    private String generatePassword() {
         password = UUID.randomUUID().toString();
 
         return password.substring(0, 8);
     }
 
-    void welcome() {
-        System.out.println("=================================");
-        System.out.println("Welcome to " + this.companyName);
-        System.out.println();
-        System.out.println("We are going to ask you for your firstname, lastname and department.");
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMailBoxCapacity() {
+        return mailBoxCapacity;
+    }
+
+    public void setMailBoxCapacity(int mailBoxCapacity) {
+        this.mailBoxCapacity = mailBoxCapacity;
+    }
+
+    public void welcome() {
+        System.out.println("=================================");
+        System.out.println();
+        System.out.println("Hello and Welcome to " + this.companyName);
+        System.out.println();
+        System.out.println("We are going to ask you for your firstname, lastname and department so we can generate and email address for you.");
+        System.out.println();
+        System.out.println("=================================");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your firstname");
+        this.firstName = scanner.nextLine();
+
+        System.out.println("Enter your lastname");
+        this.lastName = scanner.nextLine();
+
+        System.out.println("Enter your department");
+        this.department = scanner.nextLine();
+
+        System.out.println("Your email address is: " + generateEmail());
+        System.out.println();
+        System.out.println("Your password is: " + generatePassword());
+        System.out.println();
+        System.out.println("Your mail box capacity is: " + this.mailBoxCapacity);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String [] args) {
         EmployeeActions employeeActions = new EmployeeActions();
-        String pass = employeeActions.generatePassword();
 
-        System.out.println("password: " + pass);
+        employeeActions.welcome();
     }
 }
